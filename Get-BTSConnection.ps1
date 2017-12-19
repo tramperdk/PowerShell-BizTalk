d0b005c5588fe13f8ddcdd14226615e4bbd08d88
Simon <simon.lausten@gmail.com>
2017-12-19 15:01:02 +0100

    Implemented a little error handling and documentation

--- Get-BTSConnection.ps1
+++ Get-BTSConnection.ps1
@@ -1,5 +1,52 @@
-[void] [System.reflection.Assembly]::LoadWithPartialName("Microsoft.BizTalk.ExplorerOM")
-$Catalog = New-Object Microsoft.BizTalk.ExplorerOM.BtsCatalogExplorer
-$mgmtDB = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\BizTalk Server\3.0\Administration').MgmtDBName
-$sqlServer = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\BizTalk Server\3.0\Administration').MgmtDBServer
-$Catalog.ConnectionString = "SERVER=$sqlServer;DATABASE=$mgmtDB;Integrated Security=SSPI"
+<#
+.SYNOPSIS
+    Connects to Microsoft.BizTalk.ExplorerOM on localhost
+.DESCRIPTION
+    Connects to the Assembly called Microsoft.BizTalk.ExplorerOM, this assembly is found on BizTalk servers.
+.EXAMPLE
+    PS C:\> .\Get-BTSConnection.ps1
+    This example returns a [bool] and a message about connectivity.
+.INPUTS
+    N/A
+.OUTPUTS
+    $Return
+.NOTES
+    General notes
+#>
+
+try {
+    # Prep hashtable to hold our return values.
+    [hashtable]$Return = @{}
+
+    # Load ExplorerOM.
+    if([bool] [System.reflection.Assembly]::LoadWithPartialName("Microsoft.BizTalk.ExplorerOM")) {
+        $Return.Connected = $false
+        $Return.Message = "Unable to load Microsoft.BizTalk.ExplorerOM, are you running script on a BizTalk server?"
+    }
+    else {
+        $Catalog = New-Object Microsoft.BizTalk.ExplorerOM.BtsCatalogExplorer    
+    }
+    
+    # Fetch local BizTalk settings.
+    $mgmtDB    = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\BizTalk Server\3.0\Administration').MgmtDBName
+    $sqlServer = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\BizTalk Server\3.0\Administration').MgmtDBServer
+
+    if(($mgmtDB -eq $null) -or ($sqlServer -eq $null)) {
+        $Return.Connected = $false
+        $Return.Message = "mgmtDB or sqlServer variable is null, are you running script on a BizTalk server?"
+    }
+
+    # Use previously gathered information in Catalog connectionstring.
+    $Catalog.ConnectionString   = "SERVER=$sqlServer;DATABASE=$mgmtDB;Integrated Security=SSPI" 
+
+    if ($Catalog) {
+        $Return.Connected = $true
+        $Return.Message = "Connected"   
+    }
+    
+    return $Return
+}
+catch {
+    $Return.Connected = $false
+    $Return.Message = $_.Exception.Message
+}
